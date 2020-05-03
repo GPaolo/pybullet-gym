bd8ead82f3dbfe3d0684b5de79c98780620453aa
Giuseppe Laptop <giuseppe.paolo@softbankrobotics.com>
2020-05-03 12:37:08 +0200

    Made black floor

--- code_test.py
+++ code_test.py
@@ -11,11 +11,11 @@ env = gym.make('AntMuJoCoMazeEnv-v0')
 env.render()
 env.reset()
 
-for i in range(10000):
-  env.step(env.action_space.sample())
+# for i in range(10000):
+#   env.step(env.action_space.sample())
 
-# image = env.render(mode='rgb_array', top_bottom=True)
-# plt.figure()
-# plt.imshow(image)
-# plt.show()
+image = env.render(mode='rgb_array')
+plt.figure()
+plt.imshow(image)
+plt.show()
 
